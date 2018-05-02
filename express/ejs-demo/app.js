ea70f06792bfde27c2868954e9d5cee8d34a1901
maksym <maksymus@gmail.com>
2018-05-03 02:07:47 +0300

    web training - ejs posts

--- express/ejs-demo/app.js
+++ express/ejs-demo/app.js
@@ -1,8 +1,10 @@
 var express = require("express");
 var ejs = require("ejs");
+var bodyParser = require("body-parser");
 
 var app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
     res.render("home.ejs");
@@ -13,13 +15,19 @@ app.get("/question/:answer", function (req, res) {
     res.render("fall.ejs", {answer: answer});
 });
 
-app.get("/posts", function (req, res) {
-    var posts = [
-        {author: "John Doe", title: "Why EJS sucks"},
-        {author: "Kirk Smith", title: "NodeJS brings light to JS development"},
-    ];
+var posts = [
+    {author: "John Doe", title: "Why EJS sucks"},
+    {author: "Kirk Smith", title: "NodeJS brings light to JS development"},
+];
 
+app.get("/posts", function (req, res) {
     res.render("posts.ejs", {posts: posts});
+});
+
+app.post("/posts/add", function (req, res) {
+    posts.push({author: req.body.name, title: req.body.title });
+    res.redirect("/posts");
+
 });
 
 app.listen(3000, function () {
