4e1573c62439d378d3991c1e3aa8808d364b0dec
maksym <maksymus@gmail.com>
2018-05-24 22:46:38 -0400

    web training - yelp camp - comments

--- exercises/yelp-camp/app.js
+++ exercises/yelp-camp/app.js
@@ -3,39 +3,17 @@ var ejs = require("ejs");
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 
+var seedDB = require("./seeds");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
 mongoose.connect('mongodb://localhost:32772/yelp-camp');
+
+seedDB();
 
 var app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
-// schema
-var campgroundSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-var campgrounds = [
-    {name: "Paradise Campground", image: "/img/paradise_campground.jpg"},
-    {name: "Difficult Campground", image: "/img/difficult_campground.jpg"},
-    {name: "White River", image: "/img/white_river.jpg"},
-    {name: "Shady Brook Campground", image: "/img/shady_brooks_campground.jpg"},
-    {name: "The Lantern Resort", image: "/img/lantern_resort_campground.jpg"},
-];
-
-// campgrounds.forEach(function (value) {
-//     var cg = new Campground({name: value.name, image: value.image});
-//     cg.save(function (err, res) {
-//         if (err) {
-//             console.log("Error:", err);
-//         } else {
-//             console.log("Campground saved ", res);
-//         }
-//     })
-// })
 
 app.get("/", function (req, res) {
     res.render("landing.ejs");
@@ -46,21 +24,21 @@ app.get("/campgrounds", function (req, res) {
         if (err) {
             console.log("error:", err);
         } else {
-            res.render("campgrounds.ejs", {campgrounds: campgrounds});
+            res.render("campgrounds/index.ejs", {campgrounds: campgrounds});
         }
     });
 });
 
 app.get("/campgrounds/new", function (req, res) {
-    res.render("campgrounds_new.ejs");
+    res.render("campgrounds/new.ejs");
 });
 
 app.get("/campgrounds/:id", function (req, res) {
-    Campground.findOne({_id: req.params.id}, function (err, campground) {
+    Campground.findOne({_id: req.params.id}).populate("comments").exec(function (err, campground) {
         if (err) {
             console.log("error:", err);
         } else {
-            res.render("campgrounds_show.ejs", {campground: campground});
+            res.render("campgrounds/show.ejs", {campground: campground});
         }
     });
 });
@@ -80,15 +58,49 @@ app.post("/campgrounds", function (req, res) {
     })
 });
 
-// app.get("/campgrounds/:id", function (req, res) {
-//     Campground.find({id: req.params.id}, function (err, res) {
-//         // if (err) {
-//         //     console.log("error:", err);
-//         // } else {
-//         //     res.send("show page");
-//         // }
-//     });
-// });
+/**************************************
+// Comments routes
+***************************************/
+app.get("/campgrounds/:id/comments/new", function (req, res) {
+    Campground.findOne({_id: req.params.id}).populate("comments").exec(function (err, campground) {
+        if (err) {
+            console.log("error:", err);
+        } else {
+            res.render("campgrounds/comments/new.ejs", {campground: campground});
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function (req, res) {
+    Campground.findOne({_id: req.params.id}).populate("comments").exec(function (err, campground) {
+        if (err) {
+            console.log("error:", err);
+        } else {
+            Comment.create(req.body.comment, function (err, comment) {
+                if (err) {
+                    console.log("error:", err);
+                } else {
+                    campground.comments.push(comment);
+                    campground.save();
+                    res.redirect("/campgrounds/" + req.params.id);
+                }
+            });
+        }
+    });
+
+    // var name = req.body.name;
+    // var image = req.body.image;
+    // var description = req.body.description;
+    //
+    // var cg = new Campground({name: name, image: image, description: description});
+    // cg.save(function (err, res) {
+    //     if (err) {
+    //         console.log("error:", err);
+    //     } else {
+    //         res.redirect("/campgrounds");
+    //     }
+    // })
+});
 
 app.listen(3000, function () {
     console.log("server is listening");
