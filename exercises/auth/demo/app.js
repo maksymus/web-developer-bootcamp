e1ed07c10b97372570502bf71df023235b022c16
maksym <maksymus@gmail.com>
2018-05-28 21:37:55 -0400

    web training - authentication

--- exercises/auth/demo/app.js
+++ exercises/auth/demo/app.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-var passportLocal = require('passport-local');
+var PassportLocal = require('passport-local');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = require("./models/user");
@@ -24,16 +24,64 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new PassportLocal(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+////////////////////////////////////////////
+// Routes
+////////////////////////////////////////////
 
 app.get("/", function (req, res) {
     res.render("home.ejs");
 });
 
-app.get("/secret", function (req, res) {
+app.get("/secret", isLoggeIn, function (req, res) {
     res.render("secret.ejs");
 });
+
+app.get("/register", function (req, res) {
+    res.render("register.ejs");
+});
+
+app.post("/register", function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    User.register(new User({username: username}), password, function (err, user) {
+        if (err) {
+            console.log(err);
+            return res.redirect("/register");
+        }
+
+        passport.authenticate("local")(req, res, function () {
+            res.redirect("/secret");
+        });
+    });
+});
+
+app.get("/login", function (req, res) {
+    res.render("login.ejs");
+});
+
+app.post("/login", passport.authenticate("local", {
+            successRedirect: "/secret",
+            failureRedirect: "/login"
+        }),function (req, res) {
+});
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggeIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+
+    res.redirect("/login");
+}
 
 app.listen(3000, function () {
     console.log("server is listening");
